e64cc841e6e6227d4e6618dcd325ff44f826df1a
gabriel-mks <2573158@cegepoutaouais.qc.ca>
2025-11-17 10:46:17 -0500

    deuxieme push examen

--- Q2.py
+++ Q2.py
@@ -2,18 +2,13 @@ import datetime
 import locale
 locale.setlocale(locale.LC_TIME,'')
 
-def afficher_jours_examens(horaire_examen):
+def afficher_jours_examens(horaire_examen: dict) -> list[str]:
     """
     Cette fonction sert à extraire les jours de la semaines où il y a des examens
     :param horaire_examen: dictionnaire contenant les dates d'examens
     :return: une liste de jours de la semaine
     """
-    horaire_examen = {"math" : "10/12/2015",
-        "anglais" : "12/12/2025",
-        "français" : "15/12/2025"}
-    cles_indices = ["math", "anglais", "français",]
-
-    for i in enumerate(horaire_examen):
+    for i in range(len(horaire_examen)):
         jours = []
         date = datetime.datetime.strptime(horaire_examen[i], "%Y-%m-%d")
         j = date.strftime("%a")
@@ -26,4 +21,20 @@ if __name__ == '__main__':
         "anglais" : "12/12/2025",
         "français" : "15/12/2025"
     }
-    print(f"Les examens sont :", ", ".join(afficher_jours_examens(horaire_examen)))
+    print("Les examens sont :", ", ".join(afficher_jours_examens(horaire_examen)))
+
+
+
+def test_afficher_jours_examens(horaire_examen: dict) -> list[str]:
+    # Arrange
+    for i in range(len(horaire_examen)):
+        jours = []
+        date = datetime.datetime.strptime(horaire_examen[i], "%Y-%m-%d")
+        j = date.strftime("%a")
+        jours.append(j)
+        return jours
+    # Act
+    resultat_attendu = "Les examen sont : jeudi, vendredi, lundi"
+
+    # Assert
+    assert resultat_afficher_jours_examens == resultat_attendu
